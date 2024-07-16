c7b268ec2e909b497e92a1208c7e6a6c99e473d9
toroneko <y.melonpan.03@gmail.com>
2024-07-17 05:19:46 +0900

    post_contrillerを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+
   def index
     @posts = Post.all
   end
@@ -18,11 +21,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path
     else
@@ -31,7 +32,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -41,5 +41,9 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
